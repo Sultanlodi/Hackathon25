06a7d2d0aa8cf2b812019616321eae3c49038b1c
Tomiwa Aluko <tomiwaaluko02@gmail.com>
2025-09-28 02:37:06 -0400

    deployment changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configuration options can be added here
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
