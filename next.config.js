4cbe306d9337d1953e185138a0259b16ef58834d
Tomiwa Aluko <tomiwaaluko02@gmail.com>
2025-09-28 03:13:58 -0400

    vercelllll

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configuration options can be added here
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = nextConfig
